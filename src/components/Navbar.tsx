91c38f9e2632e283fc659243f5021c9b08bc028a
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-10-18 22:40:01 +0300

    added cart context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Button, Nav, Container, Navbar as NavbarBs}from 'react-bootstrap'
 import {NavLink} from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 const Navbar = () => {
+    const {openCart, cartQuantity} = useShoppingCart()
   return (
     <NavbarBs className='bg-white shadow-sm mb-3'>
         <Container>
@@ -17,19 +19,20 @@ const Navbar = () => {
                     About
                 </Nav.Link>
             </Nav>
-            <Button 
+            {cartQuantity >0 && <Button 
             style={{width: '3rem', height: '3rem', position: 'relative'}}
             variant='outline-primary'
             className='rounded-circle'
+            onClick={openCart}
             >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/></svg>
             <div 
             className='rounded-circle bg-danger d-flex justify-content-center align-items-center' 
             style={{ color: 'white', width: '1.5rem', height: '1.5rem', position: 'absolute', bottom: 0, right: 0, transform: 'translate(25%,25%)'}}
             >
-            3
+            {cartQuantity}
             </div>
-            </Button>
+            </Button>}
         </Container>
     </NavbarBs>
   )
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from 'react-bootstrap'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 import { formatCurrency } from '../utils/CurrencyFormatter'
 
 type StoreItemProps = {
@@ -9,10 +10,12 @@ type StoreItemProps = {
 }
 
 const StoreItem = ({id, name, price, imgUrl}: StoreItemProps) => {
-    const quantity = 0;
+    const {getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart} = useShoppingCart()
+
+    const quantity = getItemQuantity(id)
   return (
     <Card className='h-100'>
-        <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit: 'cover'}}/> 
+        <Card.Img variant='top' src={imgUrl} height='300px' style={{objectFit: 'cover'}}/> 
         <Card.Body className='d-flex flex-column'>
             <Card.Title
             className='d-flex justify-content-between align-items-baseline mb-4'
@@ -22,7 +25,7 @@ const StoreItem = ({id, name, price, imgUrl}: StoreItemProps) => {
             </Card.Title>
             <div className='mt-auto'>
                 {quantity === 0 ? (
-                <Button className='w-100'>+ Add To Cart</Button>
+                <Button className='w-100' onClick={() => increaseCartQuantity(id)}>+ Add To Cart</Button>
                 ) : <div 
                     className='d-flex align-items-center flex-column' 
                     style={{gap: '10px'}}
@@ -31,11 +34,11 @@ const StoreItem = ({id, name, price, imgUrl}: StoreItemProps) => {
                     className='d-flex align-items-center justify-content-center' 
                     style={{gap: '10px'}}
                     >
-                    <Button>-</Button>
+                    <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                     <div><span className='fs-3'>{quantity}</span> in cart</div>
-                    <Button>+</Button>
+                    <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                     </div>
-                    <Button variant='danger' size='sm'>Remove</Button>
+                    <Button variant='danger' size='sm' onClick={() => removeFromCart(id)}>Remove</Button>
                     </div>}
             </div>
         </Card.Body>
